35233482dd241d16b4010da53a92d93ec1d5af62
riccardo0001 <ricandreoni95@gmail.com>
2022-10-21 12:07:20 +0200

    Update main.py

--- art07-NN-optimizer/main.py
+++ art07-NN-optimizer/main.py
@@ -23,24 +23,53 @@ def load_my_data():
     
     return (x_train, y_train), (x_test, y_test)
 
+def print_image(dataset, index=0):
+    plt.imshow(dataset[index])
+    plt.show()
+    
+
 
 def main():
-    (x_train, y_train), (x_test, y_test) = load_my_data()
+    # Load data
+    (X_train, y_train), (X_test, y_test) = load_my_data()
+    # Print an example
+    #print_image(X_train, 42)
     
+    # Normalize the inputs
+    X_train = X_train / 255.
+    X_test = X_test / 255.
     
+       
+    # Define the optimizers to test
+    my_optimizers = {"Mini-batch GD":tf.keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.0),
+                     "Momentum GD":tf.keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.9),
+                     "RMS Prop":tf.keras.optimizers.RMSprop(learning_rate = 0.01, rho = 0.9),
+                     "Adam":tf.keras.optimizers.Adam(learning_rate = 0.01, beta_1 = 0.9, beta_2 = 0.999)
+        }
     
+    histories = []
+    for optimizer_name, optimizer in my_optimizers.items():
+        # Define a neural network
+        my_network = tf.keras.models.Sequential([
+                    tf.keras.layers.Flatten(),
+                    tf.keras.layers.Dense(512, activation='elu'),
+                    tf.keras.layers.Dense(256, activation='elu'),
+                    tf.keras.layers.Dense(128, activation='elu'),
+                    tf.keras.layers.Dense(64, activation='elu'),
+                    tf.keras.layers.Dense(10, activation='softmax')
+                    ])
+        
+        # Compile the model
+        my_network.compile(optimizer=optimizer,
+                           loss='sparse_categorical_crossentropy', # since labels are more than 2 and not one-hot-encoded
+                           metrics=['accuracy'])
     
+        # Train the model
+        print('Training the model with optimizer {}'.format(optimizer_name))
+        history = my_network.fit(X_train, y_train, epochs=8, validation_split=0.1, verbose=1)
+        histories.append(history)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
     
 if __name__ == '__main__':
     main()
